76011296dcd96652a2737ac63cb96110d7c3c67a
Shust <shaunshu@post.bgu.ac.il>
2023-02-24 15:04:22 +0200

    changing the classifier to work with arff fancy file format

--- DSP_3_Project/src/main/java/org/example/TrainingModel/CalculateClassifier.java
+++ DSP_3_Project/src/main/java/org/example/TrainingModel/CalculateClassifier.java
@@ -1,5 +1,6 @@
 package org.example.TrainingModel;
 
+import com.opencsv.CSVWriter;
 import org.example.AWS_Services.S3Instance;
 import software.amazon.awssdk.regions.Region;
 import weka.classifiers.functions.Logistic;
@@ -7,22 +8,29 @@ import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.converters.CSVLoader;
+import weka.core.converters.ConverterUtils;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CalculateClassifier {
-    private final String bucketOfTrainingDataName;
     private final String trainingDataFileName;
-    private S3Instance s3Instance;
+    private final S3Instance s3Instance;
     private Logistic classifier;
     String noun_pair;
 
     public CalculateClassifier(String bucketOfTrainingDataName, String trainingDataFileName) {
-        this.bucketOfTrainingDataName = bucketOfTrainingDataName;
         this.trainingDataFileName = trainingDataFileName;
-        this.s3Instance = new S3Instance(Region.SA_EAST_1, bucketOfTrainingDataName);
+        this.s3Instance = new S3Instance(Region.US_EAST_1, bucketOfTrainingDataName);
     }
 
+    @Deprecated
     private void trainModel() throws Exception {
         String trainingData = s3Instance.downloadFileContentFromS3(trainingDataFileName, "txt", "src/main/java/org/example/TrainingModel/TrainingDataFromS3");
         String[] trainingDataVectors = trainingData.split("\\n");
@@ -52,6 +60,25 @@ public class CalculateClassifier {
 
     }
 
+    public void trainModelNew() {
+        String trainingData = s3Instance.downloadFileContentFromS3(trainingDataFileName, "txt", "src/main/java/org/example/TrainingModel/TrainingDataFromS3");
+        String[] trainingDataVectors = trainingData.split("\\n");
+        String csvFilePath = "src/main/java/org/example/TrainingModel/InputFilesForTrainingModel/TrainingDataCSV.csv";
+        String arffFilePath = "src/main/java/org/example/TrainingModel/InputFilesForTrainingModel/TrainingDataARFF.arff";
+
+        try {
+            createCVSFile(trainingDataVectors, csvFilePath);
+            convertCSVToArff(csvFilePath, arffFilePath);
+            Instances data = ConverterUtils.DataSource.read(arffFilePath);
+            data.setClassIndex(data.numAttributes() - 1);
+            this.classifier = new Logistic();
+            classifier.buildClassifier(data);
+            System.out.println("[DEBUG]: " + classifier);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private Instance convertVectorInputToInstance(String vector, double label) {
         String[] trainingDataVectorValues = vector.split(",");
         int number_of_rows = trainingDataVectorValues.length;
@@ -73,5 +100,45 @@ public class CalculateClassifier {
         return classifier.classifyInstance(instance);
     }
 
+    private List<String[]> convertVectorsToList(String[] trainingDataVectors) throws IOException {
+        List<String[]> csvLines = new ArrayList<>();
+        for (String trainingDataVector : trainingDataVectors) {
+            String[] trainingDataVectorParts = trainingDataVector.split("\\t");
+            this.noun_pair = trainingDataVectorParts[0];
+            String[] trainingVector = trainingDataVectorParts[1].split(",");
+            String stringLabel = trainingDataVectorParts[2];
+            String label = stringLabel.equals("True") ? "1.0" : "0.0";
+            //concat the label to the end of the vector
+            trainingVector = java.util.Arrays.copyOf(trainingVector, trainingVector.length + 1);
+            trainingVector[trainingVector.length - 1] = label;
+
+            csvLines.add(trainingVector);
+        }
+        return csvLines;
+
+    }
+
+    private void writeVectorsToCSV(String[] trainingDataVectors, Path path) throws IOException {
+        List<String[]> csvLines = convertVectorsToList(trainingDataVectors);
+        try (CSVWriter writer = new CSVWriter(new FileWriter(path.toString()))) {
+            writer.writeAll(csvLines);
+        }
+    }
+
+    private void createCVSFile(String[] trainingDataVectors, String pathToCreateTheCSV) throws IOException, URISyntaxException {
+        Path path = Paths.get(
+                ClassLoader.getSystemResource(pathToCreateTheCSV).toURI()
+        );
+        writeVectorsToCSV(trainingDataVectors, path);
+    }
+
+    private void convertCSVToArff(String pathToCSV, String pathToArff) throws Exception {
+        String[] options = new String[4];
+        options[0] = "-T";
+        options[1] = pathToCSV;
+        options[2] = "-N";
+        options[3] = pathToArff;
+        CSVLoader.main(options);
+    }
 
 }
